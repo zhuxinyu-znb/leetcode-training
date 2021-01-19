c49eb2227bf554add6fc3c06c1953e7279ef08f3
zhuxinyu <zhuxinyu05@meituan.com>
2021-01-19 18:17:04 +0800

    feat: 27.移除元素

---
+++ array/27.移除元素.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=typescript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+function removeElement(nums: number[], val: number): number {
+    let i = 0;
+    let j = nums.length - 1;
+    while(i < j) {
+        if(nums[i] === val) {
+            let temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            j--
+        } else {
+            i++
+        }
+    }
+    if (nums[i] === val) {
+        return i
+    }
+    return i + 1
+};
+// @lc code=end
+

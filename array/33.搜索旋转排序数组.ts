869bcb53625b9eeab8a23af098ec488c3866574d
zhuxinyu-znb <18600169413@163.com>
2021-01-25 23:28:08 +0800

    feat: 33.搜索旋转排序数组

---
+++ array/33.搜索旋转排序数组.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=typescript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let start: number = 0;
+  let end: number = nums.length - 1;
+  while (start <= end) {
+    const mid = start + ((end - start) >> 1);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] >= nums[start]) {
+      if (target >= nums[start] && target <= nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (target >= nums[mid] && target <= nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+// @lc code=end

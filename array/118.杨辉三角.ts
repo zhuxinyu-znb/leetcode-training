accbf69f92999ebbef88bd4dffda242a2522ea2e
zhuxinyu-znb <18600169413@163.com>
2021-03-01 23:33:35 +0800

    feat: 118.杨辉三角

---
+++ array/118.杨辉三角.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=typescript
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+function generate(numRows: number): number[][] {
+    const ret = [];
+
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(i + 1).fill(1);
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = ret[i - 1][j - 1] + ret[i - 1][j];
+        }
+        ret.push(row);
+    }
+    return ret;
+};
+// @lc code=end
+

3dfe93076afb59a26d651b46d624eb523b16f025
zhuxinyu-znb <18600169413@163.com>
2021-03-06 16:12:36 +0800

    fix: 优化

--- array/283.移动零.ts
+++ array/283.移动零.ts
@@ -9,17 +9,20 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    let i: number = 0;
-    let j: number = 0;
-    let len: number = nums.length;
-    while(i < len) {
-        if(nums[i] !== 0) {
-            let temp = nums[j];
-            nums[j++] = nums[i];
-            nums[i] = temp;
-        }
-        i++;
+  let i: number = 0;
+  let j: number = 0;
+  let len: number = nums.length;
+  while (i < len) {
+    if (nums[i] !== 0) {
+      if (i !== j) {
+        let temp = nums[j];
+        nums[j++] = nums[i];
+        nums[i] = temp;
+      } else {
+        j++;
+      }
     }
-};
+    i++;
+  }
+}
 // @lc code=end
-

5f23b5b337df1ae0aaa025f0daf84a62180378ed
zhuxinyu-znb <18600169413@163.com>
2021-03-14 20:11:22 +0800

    feat: 206.反转链表

---
+++ linkedList/206.反转链表.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let pre = null;
+    let cur = head;
+    while(cur !== null) {
+        let next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+};
+// @lc code=end
+

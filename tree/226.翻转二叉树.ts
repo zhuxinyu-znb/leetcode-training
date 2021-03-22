35a40af42850287cf0a07c57250b93496a70e808
zhuxinyu-znb <18600169413@163.com>
2021-03-22 15:24:56 +0800

    feat: 226.翻转二叉树

---
+++ tree/226.翻转二叉树.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=typescript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(!root) return null;
+    invertTree(root.left);
+    invertTree(root.right);
+    let left = root.left;
+    let right = root.right;
+    root.right = left
+    root.left = right
+    return root;
+};
+// @lc code=end
+

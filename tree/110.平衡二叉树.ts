6a6d63e1ff64b71596b6c2e01cc9e980df2aa513
zhuxinyu-znb <18600169413@163.com>
2021-03-22 16:11:57 +0800

    feat: 110.平衡二叉树

---
+++ tree/110.平衡二叉树.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=typescript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ var isBalanced = function (root) {
+    return balanced(root) !== -1
+};
+var balanced = function (node) {
+    if (!node) return 0
+    const left = balanced(node.left)
+    const right = balanced(node.right)
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1
+    }
+    return Math.max(left, right) + 1
+}
+
+// @lc code=end
+

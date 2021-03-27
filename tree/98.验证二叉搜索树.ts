bee72359fa6204ab5a2673310f304b9f8d1df652
zhuxinyu-znb <18600169413@163.com>
2021-03-27 14:54:43 +0800

    feat: 98.验证二叉搜索树

---
+++ tree/98.验证二叉搜索树.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=typescript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ const helper = (root, lower, upper) => {
+    if (root === null) {
+        return true;
+    }
+    if (root.val <= lower || root.val >= upper) {
+        return false;
+    }
+    return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+}
+var isValidBST = function(root) {
+    return helper(root, -Infinity, Infinity);
+};
+
+// @lc code=end
+
